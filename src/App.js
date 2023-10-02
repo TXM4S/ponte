aac9710beae8e57519334e8407fbc3382b962c18
Tom McCracken <tomasmccracken@icloud.com>
2023-10-02 12:13:22 +0100

    Bring back series 2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import Home from './components/home';
 import Series1 from './components/series1';
 import Series1Product from './components/series1product';
-//import Series2 from './components/series2';
-//import Series2Product from './components/series2product';
+import Series2 from './components/series2';
+import Series2Product from './components/series2product';
 import Contact from './components/contact';
 import ComingSoon from './components/comingsoon';
 
@@ -34,10 +34,9 @@ function App() {
             case PageView.Series1ProductPage:
                 return <Series1Product setShowTitle={setShowTitle} setPage={setPage} pageview={PageView} setCurrentProductPage={setCurrentProductPage} product={currentProductPage} />;
             case PageView.Series2:
-                return <ComingSoon />;
-            //    return <Series2 setShowTitle={setShowTitle} setPage={setPage} pageview={PageView} setCurrentProductPage={setCurrentProductPage}/>;
-            //case PageView.Series2ProductPage:
-            //    return <Series2Product setShowTitle={setShowTitle} setPage={setPage} pageview={PageView} setCurrentProductPage={setCurrentProductPage} product={currentProductPage} />;
+                return <Series2 setShowTitle={setShowTitle} setPage={setPage} pageview={PageView} setCurrentProductPage={setCurrentProductPage}/>;
+            case PageView.Series2ProductPage:
+                return <Series2Product setShowTitle={setShowTitle} setPage={setPage} pageview={PageView} setCurrentProductPage={setCurrentProductPage} product={currentProductPage} />;
             case PageView.Archetype:
                 return <ComingSoon />;
             case PageView.Contact:
